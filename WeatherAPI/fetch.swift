65c2ddbb6810bc350966bd186552377353c39e84
AlanRoybal <70545295+AlanRoybal@users.noreply.github.com>
2023-04-13 23:23:41 -0500

    add function to control motor when temp above 105

--- WeatherAPI/fetch.swift
+++ WeatherAPI/fetch.swift
@@ -1,4 +1,6 @@
+import time
 import requests
+import RPi.GPIO as GPIO
 
 # replace YOUR_API_KEY with your AccuWeather API key
 api_key = 'YOUR_API_KEY'
@@ -9,15 +11,35 @@ location = 'YOUR_LOCATION'
 # create the URL for the API request
 url = f'http://dataservice.accuweather.com/currentconditions/v1/{location}?apikey={api_key}'
 
-# make the API request
-response = requests.get(url)
+# set up GPIO pins for motor control
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+pwm = GPIO.PWM(17, 100)
+pwm.start(0)
 
-# if the request was successful
-if response.status_code == 200:
-    # get the weather information from the response
-    weather_info = response.json()
-    # print the weather information
-    print(weather_info)
-else:
-    # if the request was not successful, print an error message
-    print('Error:', response.status_code)
+# create a function to read the temperature from the API and turn the motor
+def read_temp_and_turn_motor():
+    # make the API request
+    response = requests.get(url)
+    # if the request was successful
+    if response.status_code == 200:
+        # get the temperature from the response
+        weather_info = response.json()[0]
+        temperature = weather_info['Temperature']['Imperial']['Value']
+        # if the temperature is greater than 105 degrees, turn the motor
+        if temperature > 105:
+            pwm.ChangeDutyCycle(100)
+            GPIO.output(18, GPIO.HIGH)
+        else:
+            pwm.ChangeDutyCycle(0)
+            GPIO.output(18, GPIO.LOW)
+    else:
+        # if the request was not successful, print an error message
+        print('Error:', response.status_code)
+
+# repeat the function call every 5 seconds
+while True:
+    read_temp_and_turn_motor()
+    time.sleep(5)
